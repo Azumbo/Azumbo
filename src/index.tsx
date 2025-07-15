854e7be0c6efca30ef20645972f9fb7cbbdb859d
AZUMBO <azumbogames@gmail.com>
2025-07-15 21:10:56 +0200

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <main className="text-center mt-40">
+        <h1 className="text-6xl font-bold text-cyan-400">Azumbo Games</h1>
+        <p className="text-xl mt-4 text-white">Вирусные игры с итальянской душой 🇮🇹</p>
+      </main>
+    </Layout>
+  );
+}
