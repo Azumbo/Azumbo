162aded9a87ad21765db81c4f43a77d841073c44
AZUMBO <azumbogames@gmail.com>
2025-07-18 22:17:24 +0200

    feat: create pixel style landing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import './globals.css';
 import React from 'react';
+import { Press_Start_2P } from 'next/font/google';
+
+const pressStart = Press_Start_2P({
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-press-start',
+  weight: '400',
+});
 
 export const metadata = {
   title: 'AZUMBO',
@@ -8,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={pressStart.variable}>
       <body>{children}</body>
     </html>
   );
