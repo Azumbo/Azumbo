ebc267493663fc8782bfe2ec81f86171a6432be2
AZUMBO <azumbogames@gmail.com>
2025-08-14 20:14:18 +0200

    Serve TikTok verification file with .txt extension

--- pages/cornettoclicker.js
+++ pages/cornettoclicker.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import Head from 'next/head';
 import IphoneWrapper from '../components/IphoneWrapper';
 
 // Page that wraps the mini game in an iPhone frame on desktop
@@ -14,8 +15,20 @@ export default function CornettoClicker() {
   }, []);
 
   return (
-    <IphoneWrapper>
-      <iframe src="/cornettoclicker/game.html" title="Cornetto Clicker" style={{ width: '100%', height: '100%', border: 'none' }} />
-    </IphoneWrapper>
+    <>
+      <Head>
+        <meta
+          name="tiktok-developers-site-verification"
+          content="xALZu5Vzk4n3cZx80f6uIEpTEBZIs1MS"
+        />
+      </Head>
+      <IphoneWrapper>
+        <iframe
+          src="/cornettoclicker/game.html"
+          title="Cornetto Clicker"
+          style={{ width: '100%', height: '100%', border: 'none' }}
+        />
+      </IphoneWrapper>
+    </>
   );
 }
