3194a5bc4ea0e2767e0fc0ffb63e72a0f4c8e178
AZUMBO <azumbogames@gmail.com>
2025-08-24 17:03:50 +0200

    Remove obsolete IphoneWrapper wrapper

--- pages/cornettoclicker.js
+++ pages/cornettoclicker.js
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
-import IphoneWrapper from '../components/IphoneWrapper';
 
-// Page that wraps the mini game in an iPhone frame on desktop
+// Page that displays the mini game and redirects to the raw HTML on mobile devices
 export default function CornettoClicker() {
   useEffect(() => {
     // On mobile devices open the raw html page
@@ -22,13 +21,11 @@ export default function CornettoClicker() {
           content="xALZu5Vzk4n3cZx80f6uIEpTEBZIs1MS"
         />
       </Head>
-      <IphoneWrapper>
-        <iframe
-          src="/cornettoclicker/game.html"
-          title="Cornetto Clicker"
-          style={{ width: '100%', height: '100%', border: 'none' }}
-        />
-      </IphoneWrapper>
+      <iframe
+        src="/cornettoclicker/game.html"
+        title="Cornetto Clicker"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+      />
     </>
   );
 }
